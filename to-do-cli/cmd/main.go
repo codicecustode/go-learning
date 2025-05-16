ea804747cc12c90ceb97b179f5fc4586c38fd1d6
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-05-16 22:38:19 +0530

    initialize main and todo packages with command flags

---
+++ to-do-cli/cmd/main.go
@@ -0,0 +1,13 @@
+package main;
+import (
+	"fmt"
+	"flag"
+)
+
+func main() {
+
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
+	deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError	)
+	
+}
---
+++ to-do-cli/internal/todo/todo.go
@@ -0,0 +1,10 @@
+package todo
+
+
+import "flag"
+
+
+
+
+
+
