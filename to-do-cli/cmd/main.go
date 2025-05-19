edd5f6d7f54713c2112c8f4d8616822b767c8085
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-05-19 19:27:15 +0530

    refactor: clean up main.go and improve command handling for adding todos

--- to-do-cli/cmd/main.go
+++ to-do-cli/cmd/main.go
@@ -1,13 +1,35 @@
-package main;
+package main
+
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"os"
+	"todocli/internal/todo"
 )
 
 func main() {
 
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
-	updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
-	deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError	)
-	
+	//updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
+	//deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError	)
+
+	addTitle := addCmd.String("title", "", "Title of the todo")
+	addDescription := addCmd.String("description", "", "Description of the todo")
+	flag.Parse()
+
+	switch flag.Args()[0] {
+	case "add":
+		addCmd.Parse(flag.Args()[1:])
+		if *addTitle == "" || *addDescription == "" {
+			fmt.Println("Please provide both title and description")
+			os.Exit(1)
+		}
+
+		todoList := todo.TodoList{}
+		todoList.AddTodo(*addTitle, *addDescription)
+
+	case "allTodos":
+		todoList := todo.TodoList{}
+		todoList.PrintAllTodos()
+	}
 }
