9a246fbcf7798888ba2aa041ae80846468445138
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-05-21 19:18:51 +0530

    refactor: streamline AddTodo method and enhance PrintAllTodos output

--- to-do-cli/internal/todo/todo.go
+++ to-do-cli/internal/todo/todo.go
@@ -19,30 +19,20 @@ type TodoList struct {
 
 func (t *TodoList) AddTodo(title, description string) {
 	fmt.Println("Adding todo...")
-	fmt.Println("Title:", title)
-	fmt.Println("Description:", description)
 	todo := Todo {
 		ID:          len(t.Todos) + 1,
 		Title:       title,
 		Description: description,
 		Status:      "pending",
 	}
-
 	t.Todos = append(t.Todos, todo)
 	fmt.Println("Here is the added todo")
 	fmt.Println("Todo added successfully!")
-	for _, todo := range t.Todos {
-		fmt.Println("ID:", todo.ID)
-		fmt.Println("Title:", todo.Title)
-		fmt.Println("Description:", todo.Description)
-		fmt.Println("Status:", todo.Status)
-	}
 }
 
 func (t *TodoList) PrintAllTodos() {
-	fmt.Println("All Todos:")
+	fmt.Println("Here is All Todo............")
 	for _, todo := range t.Todos {
-		fmt.Println("Yes todo are there")
 		fmt.Println("ID:", todo.ID)
 		fmt.Println("Title:", todo.Title)
 		fmt.Println("Description:", todo.Description)
@@ -51,7 +41,45 @@ func (t *TodoList) PrintAllTodos() {
 	}
 }
 
+func (t *TodoList) UpdateTodo(id int, status, title, description string){
 
+	isIdFound := false
+
+	for i := range t.Todos {
+		if t.Todos[i].ID == id {
+			t.Todos[i].Status = status
+			t.Todos[i].Title = title
+			t.Todos[i].Description = description
+			isIdFound = true
+			break
+		}
+
+	}
+
+	if isIdFound {
+		fmt.Println("Todo are updated successfully.")
+	} else {
+		fmt.Println("Unable to find the specified id todo.")
+	}
+
+}
+
+func (t *TodoList ) DeleteTodo(id int){
+	deletedId := -1
+	for i, todo := range t.Todos {
+		if todo.ID == id {
+			deletedId = i
+			break
+		}
+	}
+	
+	if deletedId == -1 {
+		fmt.Println("Deleted ID not Found")
+		return
+	}
+	t.Todos = append(t.Todos[:deletedId], t.Todos[deletedId+1:]...)
+	fmt.Println("Todo deleted succesfull")
+}
 
 
 
