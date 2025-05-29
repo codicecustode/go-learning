898ab8c72ebb5f38b6df9aaadb176332c0c03f76
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-05-29 11:53:43 +0530

    refactor: improve command flag descriptions and enhance error messages in main.go

--- to-do-cli/cmd/main.go
+++ to-do-cli/cmd/main.go
@@ -8,20 +8,28 @@ import (
 )
 
 func main() {
-
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 	updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
 	deleteCmd := flag.NewFlagSet("delete", flag.ExitOnError)
 
 	addTitle := addCmd.String("title", "", "Title of the todo")
 	addDescription := addCmd.String("description", "", "Description of the todo")
-	deleteId := deleteCmd.Int("id", -1, "Give the Id for deleting todo ")
-	updateTitle := updateCmd.String("title", "", "Kindly give the title for updation")
-	updateDescription := updateCmd.String("description", "", "description for updation")
-	updateStatus := updateCmd.String("status", "", "Give status for updation")
+
+	deleteId := deleteCmd.Int("id", -1, "ID of the todo to delete")
+
+	updateTitle := updateCmd.String("title", "", "Title for update")
+	updateDescription := updateCmd.String("description", "", "Description for update")
+	updateStatus := updateCmd.String("status", "", "Status for update")
+
 	flag.Parse()
 
 	todoList := todo.TodoList{}
+
+	if len(flag.Args()) < 1 {
+		fmt.Println("Expected 'add', 'delete', 'update', or 'alltodo' subcommands")
+		os.Exit(1)
+	}
+
 	switch flag.Args()[0] {
 	case "add":
 		addCmd.Parse(flag.Args()[1:])
@@ -34,19 +42,23 @@ func main() {
 	case "delete":
 		deleteCmd.Parse(flag.Args()[1:])
 		if *deleteId == -1 {
-			fmt.Print("Kindlr Provide the Todo Id for deletion")
+			fmt.Println("Please provide the Todo ID for deletion")
+			os.Exit(1)
 		}
 		todoList.DeleteTodo(*deleteId)
 
 	case "update":
 		updateCmd.Parse(flag.Args()[1:])
-		if *updateDescription == "" || *updateTitle == "" {
-			fmt.Println("Give the title and description fo update")
-			return
+		if *updateTitle == "" || *updateDescription == "" {
+			fmt.Println("Please provide both title and description for update")
+			os.Exit(1)
 		}
 		todoList.UpdateTodo(*deleteId, *updateStatus, *updateTitle, *updateDescription)
 
 	case "alltodo":
 		todoList.PrintAllTodos()
+
+	default:
+		fmt.Println("Unknown command. Available commands: add, delete, update, alltodo")
 	}
 }
