b1775b8c74ed2968ae5615530308e83f7ecfde9b
Aman Kumar Singh <amanrkt2020@gmail.com>
2026-06-18 09:36:41 +0530

    register all route in entry server file (main.go)

--- rest-api/bookstore-api/cmd/api/main.go
+++ rest-api/bookstore-api/cmd/api/main.go
@@ -2,7 +2,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+
 	"bookstore-api/internal/config"
+	"bookstore-api/internal/handler"
+	"bookstore-api/internal/repository"
+	"bookstore-api/internal/service"
+
 	"github.com/go-chi/chi/v5"
 )
 
@@ -14,11 +19,21 @@ func main() {
 		fmt.Println("Error loading in config:", err)
 	}
 
+	repo := repository.NewBookRepository()
+	svc := service.NewBookService(repo)
+	h := handler.NewBookHandler(svc)
+
 	r := chi.NewRouter()
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 	})
+
+	r.Get("/books", h.GetAll)
+	r.Get("/books/{id}", h.GetByID)
+	r.Post("/books", h.Create)
+	r.Patch("/books/{id}", h.Update)
+	r.Delete("/books/{id}", h.Delete)
 
 	fmt.Printf("Server starting on port %s\n", cfg.Serverport)
 
